524866b6aec770b884fe57d7bd3452cbde19d817
Jean Jonethal <jean.jonethal@gigaset.com>
2014-09-08 14:29:10 +0200

    fix luajit 86 -> 64
    Signed-off-by: Jean Jonethal <jean.jonethal@gigaset.com>

--- make_win_x64.cmd
+++ make_win_x64.cmd
@@ -1,11 +1,11 @@
-mkdir buildx64
 call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.Cmd" /x64 /release
 ::set INCLUDE=D:\work\luars232\librs232.git\include;E:\luajit\x64;%INCLUDE%
 set INCLUDE=D:\work\luars232\librs232.git\include;E:\luajit\x64;%INCLUDE%
+mkdir buildx64
 cd buildx64
 cl /O2 /homeparams /DUNICODE /D_UNICODE /DWIN32 /DRS232_EXPORT ..\bindings\lua\luars232.c ..\src\rs232.c ..\src\rs232_windows.c E:\luajit\x64\lua51.lib /O2 /LD /link /dll
 pause
 copy ..\bindings\lua\test.lua .
-E:\luajit\x86\luajit test.lua
+E:\luajit\x64\luajit test.lua
 pause
 
