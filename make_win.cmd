9be0245c7997a2bd7739034cf3c3130e677d1bf0
Jean Jonethal <jjonethal@yahoo.de>
2016-12-06 23:57:41 +0100

    vs14

--- make_win.cmd
+++ make_win.cmd
@@ -2,11 +2,12 @@ mkdir build
 
 IF "%TARGET%"  == "" SET TARGET=x86
 IF '%WIN_SDK%' == '' SET WIN_SDK="C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.Cmd"
-IF "%LUA_LIB%" == "" SET LUA_LIB=E:\luajit\%TARGET%
-call %WIN_SDK% /%TARGET% /release
+IF "%LUA_LIB%" == "" SET LUA_LIB=c:\app\lua
+:: call %WIN_SDK% /%TARGET% /release
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 
 ::set INCLUDE=D:\work\luars232\librs232.git\include;E:\luajit\x86;E:\luajit\luajit-2.0\src;%INCLUDE%
-set INCLUDE=%CD%\include;%LUA_LIB%;%INCLUDE%
+set INCLUDE=%CD%\include;%LUA_LIB%;%INCLUDE%;C:\app\lua\include
 pushd build
 cl /O2 /DUNICODE /D_UNICODE /DWIN32 /DRS232_EXPORT ..\bindings\lua\luars232.c ..\src\rs232.c ..\src\rs232_windows.c %LUA_LIB%\lua51.lib /O2 /LD /link /dll
 pause
