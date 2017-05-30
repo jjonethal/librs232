80367a4f0f09020136504fb3e5ce6f119aa95d65
Jean Jonethal <jean.jonethal@gigaset.com>
2017-05-30 12:45:52 +0200

    fix lua prototypes for luaL_Reg conversion from 5.0 to 5.1 naming

--- make_win.cmd
+++ make_win.cmd
@@ -3,13 +3,15 @@ mkdir build
 IF "%TARGET%"  == "" SET TARGET=x86
 IF '%WIN_SDK%' == '' SET WIN_SDK="C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.Cmd"
 IF "%LUA_LIB%" == "" SET LUA_LIB=c:\app\lua
-:: call %WIN_SDK% /%TARGET% /release
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+call %WIN_SDK% /%TARGET% /release
+:: call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 
 ::set INCLUDE=D:\work\luars232\librs232.git\include;E:\luajit\x86;E:\luajit\luajit-2.0\src;%INCLUDE%
-set INCLUDE=%CD%\include;%LUA_LIB%;%INCLUDE%;C:\app\lua\include
+set INCLUDE=%CD%\include;%LUA_LIB%;%INCLUDE%;%LUA_LIB%\include
+echo INCLUDE_PATH: %INCLUDE%
 pushd build
-cl /O2 /DUNICODE /D_UNICODE /DWIN32 /DRS232_EXPORT ..\bindings\lua\luars232.c ..\src\rs232.c ..\src\rs232_windows.c %LUA_LIB%\lua51.lib /O2 /LD /link /dll
+@echo on
+cl /O2 /DUNICODE /D_UNICODE /DWIN32 /D_WIN32 /DRS232_EXPORT /D_WINDOWS /D_USRDLL /Dlibrs232lua_EXPORTS ..\bindings\lua\luars232.c ..\src\rs232.c ..\src\rs232_windows.c %LUA_LIB%\lua51.lib /O2 /LD /link /dll
 pause
 copy ..\bindings\lua\test.lua .
 ::E:\luajit\x86\luajit test.lua
